08f3b5e06e2d87b1e1912b7f804f852d7a525c84
Daniel Füvesi <e1326576@student.tuwien.ac.at>
2018-05-06 15:59:05 +0200

    add build.gradle, gradle wrapper; add dummy Main class

---
+++ src/main/kotlin/at/ac/tuwien/otl/evrptw/Main.kt
@@ -0,0 +1,20 @@
+package at.ac.tuwien.otl.evrptw
+
+/**
+ * <h4>About this class</h4>
+ *
+ * <p>Description</p>
+ *
+ * @author Daniel Fuevesi
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+class Main {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World")
+        }
+    }
+}
