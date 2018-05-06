df7a6c14d9ac9833dd3c40800fa84f1ca805a198
Daniel Füvesi <e1326576@student.tuwien.ac.at>
2018-05-06 16:14:20 +0200

    add sample test

---
+++ src/test/kotlin/at/ac/tuwien/otl/evrptw/SampleTest.kt
@@ -0,0 +1,21 @@
+package at.ac.tuwien.otl.evrptw
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+/**
+ * <h4>About this class</h4>
+ *
+ * <p>Description</p>
+ *
+ * @author Daniel Fuevesi
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+class SampleTest {
+
+    @Test
+    internal fun sampleTest() {
+        Assertions.assertEquals(4, 2 + 2)
+    }
+}
